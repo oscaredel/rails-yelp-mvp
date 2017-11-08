6baf4ded774bb40de6f9ed37b1f1ffbaaa93ab45
Oscar Edel <edeloscar@gmail.com>
2017-11-08 17:30:54 +0100

    Created Review controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,21 +1,25 @@
 class ReviewsController < ApplicationController
   before_action :set_restaurant
-  def index
-    @reviews = Review.all
-  end
 
   def new
     @review = Review.new
   end
 
+  def index
+    @reviews = Review.all
+  end
+
   def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new(review_params)
-    if @review.save
-      redirect_to restaurant_path(@restaurant)
+    @review.restaurant = @restaurant
+
+   if @review.save
+     redirect_to restaurant_path(@restaurant)
     else
       render :new
     end
-  end
+ end
 
   private
 
@@ -24,6 +28,6 @@ class ReviewsController < ApplicationController
   end
 
   def review_params
-    params.require(:review).permit(:content)
+    params.require(:review).permit(:content, :rating)
   end
 end
